0e3898a25d22ecea08d6e50b9d320e0aac84b738
alexgonzalez001 <alexgr20063@gmail.com>
2025-10-23 13:18:58 +0200

    v.0.2 saludo a la persona

--- src/App.java
+++ src/App.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+import java.lang.Object;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hi! I am Alex");
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Ingresa tu nombre: ");
+        String nombre = entrada.nextLine();
+
+        System.out.println("¡Hola, " + nombre + "! Bienvenido.");
+
+        entrada.close();
 
     }
 }
